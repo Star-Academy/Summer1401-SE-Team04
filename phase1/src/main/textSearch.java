0ef98e1ea83f896115c1d3f17727975281543da7
abdossamad haghiri <abdossamadhaghiri@gmail.com>
2022-07-17 15:46:40 +0430

    merge

--- phase1/src/main/textSearch.java
+++ phase1/src/main/textSearch.java
@@ -4,9 +4,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 public class textSearch {
     public static HashMap<String, List<String>> invertedIndex = new HashMap<String, List<String>>();
@@ -30,48 +31,48 @@ public class textSearch {
 
             }
         }
-
-
+    }
 
     public static void addTextToInvertedIndex(String word, String fileName) {
-        try {
-            List<String> files = invertedIndex.get(word);
-            if (files == null) {
-                List<String> f = new ArrayList<>();
-                f.add(fileName);
-                invertedIndex.put(word, f);
-                return;
-            } else {
-                for (String f : files) {
-                    if (f.equals(fileName)) {
-                        return;
-                    }
 
+        List<String> files = invertedIndex.get(word);
+        if (files == null) {
+            List<String> f = new ArrayList<>();
+            f.add(fileName);
+            invertedIndex.put(word, f);
+            return;
+        } else {
+            for (String f : files) {
+                if (f.equals(fileName)) {
+                    return;
                 }
+
             }
-            files.add(fileName);
-
-        } catch (Exception ignored) {
-
         }
+        files.add(fileName);
+
+
     }
 
     public static void main(String[] args) {
-        //readDoc();
-        addTextToInvertedIndex("tt", "abc");
-        addTextToInvertedIndex("tt", "abcd");
-        addTextToInvertedIndex("bb", "qwerty");
-        Scanner sc = new Scanner(System.in);
+        try {
+            readDoc();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Scanner scanner = new Scanner(System.in);
         System.out.println("please enter the word");
-        String word = sc.nextLine();
-        List<String> f = invertedIndex.get(word);
-        if (f == null) {
-            System.out.println("word was not found!!");
+        String word = scanner.nextLine();
+        List<String> fileNames = invertedIndex.get(word);
+        if (fileNames == null) {
+            System.out.println("word was not found!");
             return;
         }
 
-        for (String s : f) {
-            System.out.println(s);
+        for (String fileName : fileNames) {
+            System.out.println(fileName);
         }
     }
 }
+
