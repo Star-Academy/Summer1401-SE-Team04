78c90b3b0d750efac82ef6594260aae20a532f7d
abdossamad haghiri <abdossamadhaghiri@gmail.com>
2022-07-17 15:36:55 +0430

    complete readDoc

--- phase1/src/main/textSearch.java
+++ phase1/src/main/textSearch.java
@@ -1,17 +1,50 @@
-import java.util.List;
+package main;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class textSearch {
-    public  HashMap<String, List<String>> invertedIndex = new HashMap<String, List<String>>();
-    public String directory ="";
-    public static void readDoc(){
+    public static HashMap<String, List<String>> invertedIndex = new HashMap<String, List<String>>();
+    public static String directoryPath = "E:\\java\\folder";
+
+    public static void readDoc() throws IOException {
+
+        final File directory = new File(directoryPath);
+        for (final File file : directory.listFiles()) {
+            String fileName = file.getName();
+            if (!file.isDirectory()) {
+                FileReader fileReader = new FileReader(file.getPath());
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+                String text = bufferedReader.readLine().trim().toUpperCase();
+                text = text.replaceAll("(\\W+)", " ").trim();
+
+                String[] words = text.split(" ");
+                for (String word : words) {
+                    addTextToInvertedIndex(word, fileName);
+                }
+
+            }
+        }
+
+
 
     }
-    public static void addTextToInvertedIndex(String text, String fileName){
+    public static void addTextToInvertedIndex(String word, String fileName) {
 
     }
+
     public static void main(String[] args) {
-        readDoc();
+
+        try {
+            readDoc();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
