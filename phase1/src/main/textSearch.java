907952c9e0fce3b3a13cd5b0705e2d277f5e6364
mahdi <acilic79@gmail.com>
2022-07-17 13:17:57 +0430

    initial commit

---
+++ phase1/src/main/textSearch.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.HashMap;
+
+public class textSearch {
+    public  HashMap<String, List<String>> invertedIndex = new HashMap<String, List<String>>();
+    public String directory ="";
+    public static void readDoc(){
+
+    }
+    public static void addTextToInvertedIndex(String text, String fileName){
+
+    }
+    public static void main(String[] args) {
+        readDoc();
+
+    }
+}
