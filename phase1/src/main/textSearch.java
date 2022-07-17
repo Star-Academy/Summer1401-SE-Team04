a8a39ecad86286adb1fed226ba7ca67ca15db22c
mahdi <acilic79@gmail.com>
2022-07-17 15:15:25 +0430

    addTextToInvertedIndex done

--- phase1/src/main/textSearch.java
+++ phase1/src/main/textSearch.java
@@ -1,17 +1,59 @@
-import java.util.List;
-import java.util.HashMap;
+import java.util.*;
 
 public class textSearch {
-    public  HashMap<String, List<String>> invertedIndex = new HashMap<String, List<String>>();
-    public String directory ="";
-    public static void readDoc(){
+    public static HashMap<String, List<String>> invertedIndex = new HashMap<>();
+    public String directory = "";
 
-    }
-    public static void addTextToInvertedIndex(String text, String fileName){
+    public static void readDoc() {
+        try {
+            String word = null, fileName = null;
 
+
+            addTextToInvertedIndex(word, fileName);
+        } catch (Exception ignored) {
+
+        }
     }
+
+    public static void addTextToInvertedIndex(String word, String fileName) {
+        try {
+            List<String> files = invertedIndex.get(word);
+            if (files == null) {
+                List<String> f = new ArrayList<>();
+                f.add(fileName);
+                invertedIndex.put(word, f);
+                return;
+            } else {
+                for (String f : files) {
+                    if (f.equals(fileName)) {
+                        return;
+                    }
+
+                }
+            }
+            files.add(fileName);
+
+        } catch (Exception ignored) {
+
+        }
+    }
+
     public static void main(String[] args) {
-        readDoc();
+        //readDoc();
+        addTextToInvertedIndex("tt", "abc");
+        addTextToInvertedIndex("tt", "abcd");
+        addTextToInvertedIndex("bb", "qwerty");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("please enter the word");
+        String word = sc.nextLine();
+        List<String> f = invertedIndex.get(word);
+        if (f == null) {
+            System.out.println("word was not found!!");
+            return;
+        }
 
+        for (String s : f) {
+            System.out.println(s);
+        }
     }
 }
