5565e10972ee2c01e07a4b02ef9e86ce75d6bc61
mahdi <acilic79@gmail.com>
2022-07-20 10:49:03 +0430

    second question Done

--- phase1/src/main/textSearch.java
+++ phase1/src/main/textSearch.java
@@ -4,10 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class textSearch {
 
@@ -65,19 +62,74 @@ public class textSearch {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("please enter the word");
-        String word = scanner.nextLine().toUpperCase();
-        List<String> fileNames = invertedIndex.get(word);
-        if (fileNames == null) {
+        System.out.println("please enter the words");
+        String words = scanner.nextLine().toUpperCase();
+        List<String> searchWords = List.of(words.split(" "));
+        List<String> finalResult = new ArrayList<>();
+        HashMap<String,List<String>> positiveSearchWords = new HashMap<>();
+        HashMap<String,List<String>> negativeSearchWords = new HashMap<>();
+        HashMap<String,List<String>> normalSearchWords = new HashMap<>();
+        for (String searchWord : searchWords) {
+            if (searchWord.startsWith("+")){
+                searchWord = searchWord.substring(1);
+                positiveSearchWords.put(searchWord,invertedIndex.get(searchWord));
+            }else if(searchWord.startsWith("-")){
+                searchWord = searchWord.substring(1);
+                negativeSearchWords.put(searchWord,invertedIndex.get(searchWord));
+            }else {
+                normalSearchWords.put(searchWord,invertedIndex.get(searchWord));
+            }
+        }
+        Map.Entry<String,List<String>> firstNormalSearchWord = normalSearchWords.entrySet().iterator().next();
+        if (firstNormalSearchWord.getValue() == null){
+            System.out.println("word was not found!");
+            return;
+        }
+        for (String fileName : firstNormalSearchWord.getValue()) {
+
+            boolean flag = true;
+            for (List<String> normalList : normalSearchWords.values()) {
+                if (normalList == null) {continue;}
+                if (!normalList.contains(fileName)) {
+                    flag = false;
+                    break;
+                }
+            }
+            for (List<String> negativeList : negativeSearchWords.values()) {
+                if (negativeList == null) {continue;}
+                if (negativeList.contains(fileName)) {
+                    flag = false;
+                    break;
+                }
+            }
+            boolean positiveFlag = false;
+            for (List<String> positiveList : positiveSearchWords.values()) {
+                if (positiveList == null) {continue;}
+                if (positiveList.contains(fileName)) {
+                    positiveFlag = true;
+                    break;
+                }
+            }
+            if (!positiveFlag && positiveSearchWords.size() != 0){
+                flag = false;
+            }
+            if (flag){
+                finalResult.add(fileName);
+            }
+        }
+
+
+
+        if (finalResult.size() == 0) {
             System.out.println("word was not found!");
             return;
         }
 
-        for (String fileName : fileNames) {
+        for (String fileName : finalResult) {
             System.out.println(fileName);
         }
+
     }
 }
 
