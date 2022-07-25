505291c255a06ef24bb632e69c5e9b177a952fb6
abdossamad haghiri <abdossamadhaghiri@gmail.com>
2022-07-25 15:58:00 +0430

    Deserialize

--- phase3/Models.cs
+++ phase3/Models.cs
@@ -1,2 +1,21 @@
 namespace phase3.Models
+{
+    public class Student
+{
+    public int StudentNumber { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
+
+public class Grade
+{
+    public int StudentNumber { get; set; }
+
+    public string? Lesson { get; set; }
+
+    public double Score {get; set; }
+
+}
+
+}
 
--- phase3/Program.cs
+++ phase3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using phase3.Models;
+using System.Text.Json;
+namespace phase3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string studentsJson = System.IO.File.ReadAllText(@"students.json");
+            string gradesJson = System.IO.File.ReadAllText(@"scores.json");
+
+            List<Student> students = JsonSerializer.Deserialize<List<Student>>(studentsJson);
+            List<Grade> grades = JsonSerializer.Deserialize<List<Grade>>(gradesJson);
+
+            
+        }
+    }
+}
