724eb4599dea6534b4a7b414a13ca89e7aea20af
mahdi <acilic79@gmail.com>
2022-07-31 17:51:31 +0430

    initial commit

---
+++ phase5/SimpleSearch/AdvanceSearch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSearch
+{
+    public class AdvanceSearch
+    {
+        public List<string> GetFileNamesBySearchWords(List<string> searchWords, Dictionary<string, List<string>> invertedIndex)
+        {
+            return new List<string>();
+        }
+    }
+}
---
+++ phase5/SimpleSearch/FileReader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSearch
+{
+    public class FileReader
+    {
+        public Dictionary<string, List<string>> ReadDocs(string directoryPath)
+        {
+            DirectoryInfo d = new DirectoryInfo(directoryPath);
+            var result = new Dictionary<string, List<string>>();
+            foreach (var file in d.GetFiles())
+            {
+                var fileName = file.Name;
+                var text = File.ReadAllText(file.FullName);
+                if (text == null || text == "")
+                {
+                    continue;
+                }
+                text = text.ToUpper().Trim();
+                var sb = new StringBuilder();
+                foreach (char c in text)
+                {
+                    if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') ||  c==' ')
+                    {
+                        sb.Append(c);
+                    }
+                }
+                text = sb.ToString();
+
+                var words = text.Split(" ").ToList();
+
+
+                result.Add(fileName, words);
+            }
+            return result;
+        }
+    }
+}
