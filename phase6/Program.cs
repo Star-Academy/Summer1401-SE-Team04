806c4b206bf8f6aeeff3674d5290108ada677e96
mahdi <acilic79@gmail.com>
2022-08-15 14:23:06 +0430

    UI added

--- phase6/Program.cs
+++ phase6/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SimpleSearch;
+var directoryPath = "C:\\work\\internship\\Summer1401-SE-Team04\\phase5\\DataBase";
+
+var wordsInFiles = new FileReader().ReadDocs(directoryPath);
+var invertedIndex = new InvertedIndex().MakeInvertedIndex(wordsInFiles);
+var inputString = Console.ReadLine().ToUpper();
+var searchWords = inputString.Split(' ').ToList();
+var finalRes = new AdvanceSearch().GetFileNamesBySearchWords(searchWords, invertedIndex);
+
+if (finalRes == null || finalRes.Count == 0)
+{
+    Console.WriteLine("words was not found!");
+    return;
+}
+
+foreach (var fileName in finalRes)
+{
+    Console.WriteLine(fileName);
+}
